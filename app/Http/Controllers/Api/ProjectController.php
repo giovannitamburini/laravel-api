9926767bcea311075346f625ff5a0f537809aad3
giovannitamburini <giovatambu@gmail.com>
2023-05-31 18:00:30 +0200

    added route of show in  api.php & show function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -22,4 +22,33 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
+
+    public function show($slug)
+    {
+        // controllo di ottenere lo slug
+        // dd($slug);
+
+        // per prendere effettivamente solo il risultato devo aggiungere ->first()
+        // dopo il where possiamo aggiunegere anche altri filtri(orderby....)
+        // inn questo caso è cme scrivere: SELECT * FROM projects WHERE slug = $slug
+        $project = Project::where('slug', $slug)->first();
+
+        // dd($project);
+
+        // il risultato di projects, quando mettiamo uno slug che non esiste, è uguele a "null" 
+        if ($project) {
+
+            // inviamolo come json per poterlo passare ad axios
+            return response()->json([
+                'success' => true,
+                'projects' => $project,
+            ]);
+        } else {
+
+            return response()->json([
+                'success' => false,
+                'error' => "Il progetto non esiste",
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('projects', [ProjectController::class, 'index']);
+
+// rotta della show del progetto in vue
+// {slug} = variabile, in questo caso ci serve lo slug per comporre la rotta (adesso lo devo passare al controller)
+// (se si apre la lista delle rotte si vedrà la stessa cosa per alcune rotte)
+Route::get('projects/{slug}', [ProjectController::class, 'show']);
